e75b888cf585502e3664585cfa256f10af59892f
Esko Cruzeta <eskocruz@users.noreply.github.com>
2015-10-09 11:36:47 -0700

    handlebars logic & button handlers

--- js/app.js
+++ js/app.js
@@ -25,6 +25,23 @@
             rendered = compiled({dogs: DogPack.dogs, language: language});
 
         $('#theDogs').html(rendered);
+		attachDogButtons();
+		attachNotDogButtons();
     }
+
+	function attachDogButtons() {
+		$('.dog-button').on('click', function () {
+			var id = $(this).closest('.dog-card').data('dog-id');
+			DogPack.chooseDog(id);
+			renderDogs();
+		})
+	}
+	function attachNotDogButtons() {
+		$('.not-dog-button').on('click', function () {
+			var id = $(this).closest('.dog-card').data('dog-id');
+			DogPack.chooseNotDog(id);
+			renderDogs();
+		})
+	}
 
 })();
